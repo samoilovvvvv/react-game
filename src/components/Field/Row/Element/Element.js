0e491d089e5d9d76ce7a86f579f27190faf4de87
Ilya Samoilov <Ilyxas65@gmail.com>
2021-02-28 01:23:13 +0300

    feat: add Element component

---
+++ src/components/Field/Row/Element/Element.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Element.scss'
+
+const Element = props => {
+  return (
+    <button className={'Element'}>
+    
+    </button>
+  )
+}
+
+export default Element
