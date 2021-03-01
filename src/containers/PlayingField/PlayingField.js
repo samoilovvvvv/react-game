f7cf61d7503e16d1c9a4e1efca1068b3606ab256
Ilya Samoilov <Ilyxas65@gmail.com>
2021-03-01 16:55:23 +0300

    feat: add random for mines coordinates

--- src/containers/PlayingField/PlayingField.js
+++ src/containers/PlayingField/PlayingField.js
@@ -10,6 +10,8 @@ export default class PlayingField extends Component {
     
     this.state = {
       field: {
+        minesCoordinates: [],
+        mines: 10,
         rows: 9,
         columns: 9
       }
@@ -20,10 +22,60 @@ export default class PlayingField extends Component {
     const rows = []
     
     for(let i = 0; i < amount; i++) {
-      rows.push(<Row key={i} amount={this.state.field.columns}/>)
+      rows.push(<Row
+        key={i}
+        amount={this.state.field.columns}
+        onClick={event => this.clickElementHandler(event)}
+        x={i + 1}
+        minesCoordinates={this.state.field.minesCoordinates}
+      />)
     }
     
     return rows
+  }
+  
+  randomCoordinates() {
+    const coordinates = []
+  
+    for (let i = 0; i < this.state.field.mines; i++) {
+      let x = Math.floor(1 + Math.random() * (this.state.field.rows + 1 - 1))
+      let y = Math.floor(1 + Math.random() * (this.state.field.columns + 1 - 1))
+      
+      if (coordinates.length === 0) {
+        coordinates.push({x, y})
+        continue
+      }
+      
+      coordinates.map(element => {
+        while (x === element.x && y === element.y) {
+          x = Math.floor(1 + Math.random() * (this.state.field.rows + 1 - 1))
+          y = Math.floor(1 + Math.random() * (this.state.field.columns + 1 - 1))
+        }
+        
+        return undefined
+      })
+      
+      coordinates.push({x, y})
+    }
+  
+    return coordinates
+  }
+  
+  componentDidMount() {
+    const field = {...this.state.field}
+    let minesCoordinates = this.randomCoordinates()
+    
+    field["minesCoordinates"] = minesCoordinates
+    
+    this.setState({
+      field
+    })
+  }
+  
+  clickElementHandler(event) {
+    console.log(event.target)
+    console.log(event.target.dataset.x)
+    console.log(event.target.dataset.y)
   }
   
   render() {
