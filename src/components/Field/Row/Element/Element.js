0d2bab0606c9690ca9c6f8637592df9bd6feb6d0
Ilya Samoilov <Ilyxas65@gmail.com>
2021-03-02 02:40:40 +0300

    feat: add mine in Element

--- src/components/Field/Row/Element/Element.js
+++ src/components/Field/Row/Element/Element.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import './Element.scss'
+import mine from '../../../../img/mine.png'
 
 const Element = props => {
+  const cls = ['Element']
   let comparison = false
   
-  for (let i of props.minesCoordinates) {
-    if(props.x === i.x && props.y === i.y) {
-      comparison = true
-      break
+  for (let key in props.itemState) {
+    if((String(props.x) + String(props.y)) === key) {
+      cls.push(props.itemState[key])
+  
+      for (let i of props.minesCoordinates) {
+        if(props.x === i.x && props.y === i.y) {
+          comparison = true
+          break
+        }
+      }
     }
   }
   
   return (
-    <button
-      className={'Element'}
+    <div
+      className={cls.join(' ')}
       onClick={props.onClick}
       data-x={props.x}
       data-y={props.y}
     >
-      {comparison ? 1 : 0}
-    </button>
+      {comparison ? <img alt={'mine'} src={mine}/> : null}
+    </div>
   )
 }
 
