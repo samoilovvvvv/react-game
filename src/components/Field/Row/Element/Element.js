c15f2bcc61546b174af797c542408f3c7eb74b8d
Ilya Samoilov <Ilyxas65@gmail.com>
2021-03-01 16:51:38 +0300

    feat: add logic to element component

--- src/components/Field/Row/Element/Element.js
+++ src/components/Field/Row/Element/Element.js
@@ -2,9 +2,23 @@ import React from 'react'
 import './Element.scss'
 
 const Element = props => {
+  let comparison = false
+  
+  for (let i of props.minesCoordinates) {
+    if(props.x === i.x && props.y === i.y) {
+      comparison = true
+      break
+    }
+  }
+  
   return (
-    <button className={'Element'}>
-    
+    <button
+      className={'Element'}
+      onClick={props.onClick}
+      data-x={props.x}
+      data-y={props.y}
+    >
+      {comparison ? 1 : 0}
     </button>
   )
 }
